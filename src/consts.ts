096114723bc3309a17008f6cc99b0635ce237cc9
up-the-hill <aj.sth.2022@gmail.com>
2026-02-22 21:32:16 +1100

    added per page descriptions

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "avijya";
-export const SITE_DESCRIPTION = "Personal blog and website of Avijya Shrestha";
+export const SITE_DESCRIPTION = "Personal blog and website of Avijya Shrestha. Projects and articles on web and game development!";
